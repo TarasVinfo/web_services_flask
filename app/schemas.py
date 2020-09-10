3e104ef7600d323d26ae895d85f4a874637a96c7
TarasV <taras.vovnjanko@gmail.com>
2020-09-10 15:14:21 +0300

    add serializer schema

---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields
+from . import models
+
+
+class UserSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = models.User
+        exclude = 'id',
+        load_instance = True
+
+
+class AuthorSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = models.User
+        exclude = 'id', 'password',
+        load_instance = True
+
+
+class PostSchema(SQLAlchemyAutoSchema):
+    author = fields.Nested(AuthorSchema)
+
+    class Meta:
+        model = models.Post
+        exclude = 'id',
+        load_instance = True
