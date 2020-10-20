f29c34d751a9f29cb58267be9bcea37922dc1e64
trs <taras.vovnjanko@gmail.com>
2020-10-20 13:04:16 +0300

    changed readme. added auth module, released get-put-delete posts, released create user

--- README.md
+++ README.md
@@ -1,16 +1,53 @@
-# Study Project.
+## Training project that introduces the use of a flask framework for developing web-services.
 
 Source from - https://github.com/denyszamiatin/blog_1
 
-Technology stack.
+### Technology stack.
+    - Flask
+    - Flask-Migrate
+    - Flask-RESTful
+    - Flask-SQLAlchemy
 
-- Flask
-- Flask-Migrate
-- Flask-RESTful
-- Flask-SQLAlchemy
+### Start project.
+Initialize migration repository, runs at once.
+```bash
+flask db init
+```
+Creating migrations.
+```bash
+flask db migrate
+```
+Run migrations.
+```bash
+flask db upgrade
+```
+Run application.
+```bash
+flask run
+```
 
-Install.
-
-1) flask db init - command initialize migration repository runs at once.
-2) flask db migrate - command for creating migrations.
-3) flask db upgrade - command run migrations.
+### curl commands.
+create user:
+```bash
+curl -X POST http://localhost:5000/users -v -H "Content-type: application/json" -d '{"login": "username", "email": "username@example.com", "password": "P@ssw0rd"}'
+```
+login and obtain token:
+```bash
+curl http://localhost:5000/login --user username:P@ssw0rd -v
+```
+get all posts:
+```bash
+curl http://localhost:5000/posts -v -H "X-Api-Key: <obtained.token.after_login>"
+```
+create post:
+```bash
+curl -X POST http://localhost:5000/posts -v -H "X-Api-Key: <obtained.token.after_login>" -H "Content-type: application/json" -d '{"title": "Title Post", "body": "This post created for testing app", "date": "2020-10-01T00:00"}'
+```
+get post:
+```bash
+curl http://localhost:5000/posts/<uuid> -v
+```
+update post:
+```bash
+curl -X PUT http://localhost:5000/posts/<uuid> -H "Content-type: application/json" -d '{"title": "Title Post (changed)"}' -v
+```
--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,89 @@
-from datetime import datetime
 from flask import request
 from flask_restful import Resource
-from . import api, models, db
+from marshmallow.exceptions import ValidationError
+from sqlalchemy.exc import IntegrityError
+from . import api, db, models, schemas, auth
+
+
+post_schema = schemas.PostSchema()
+user_schema = schemas.UserSchema()
 
 
 class PostListApi(Resource):
-    def get(self):
-        return []
+    @auth.token_required
+    def get(self, user):
+        posts = db.session.query(models.Post).all()
+        return post_schema.dump(posts, many=True)
 
-    def post(self):
-        request_data = request.json
-        post = models.Post(
-            title=request_data.get('title'),
-            body=request_data.get('body'),
-            date=datetime.strptime(request_data.get('date'), '%Y-%m-%dT%H:%M'))
+    @auth.token_required
+    def post(self, user):
+        try:
+            post = post_schema.load(request.json, session=db.session)
+
+        except ValidationError as error:
+            return {'message': str(error)}, 400
+
+        post.author = user
         db.session.add(post)
         db.session.commit()
 
-        return {
-                    'title': post.title,
-                    'body': post.body,
-                    'date': str(post.date),
-                    'uuid': post.uuid
-                }, 201
+        return post_schema.dump(post), 201
 
 
 class PostApi(Resource):
     def get(self, uuid):
-        return ''
+        post = db.session.query(models.Post).filter_by(uuid=uuid).first()
+
+        if post is None:
+            return '', 404
+
+        return post_schema.dump(post)
 
     def put(self, uuid):
-        return ''
+        post = db.session.query(models.Post).filter_by(uuid=uuid).first()
+
+        if post is None:
+            return '', 404
+
+        post = post_schema.load(request.json, instance=post,
+                                session=db.session)
+
+        db.session.add(post)
+        db.session.commit()
+
+        return post_schema.dump(post)
 
     def delete(self, uuid):
+        post = db.session.query(models.Post).filter_by(uuid=uuid).first()
+
+        if post is None:
+            return '', 404
+
+        db.session.delete(post)
+        db.session.commit()
+
         return '', 204
+
+
+class UserListApi(Resource):
+
+    def post(self):
+        try:
+            user = user_schema.load(request.json, session=db.session)
+
+        except ValidationError as error:
+            return {'message': str(error)}, 400
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+
+        except IntegrityError:
+            return {'message': 'User exists'}, 409
+
+        return user_schema.dump(user), 201
 
 
 api.add_resource(PostListApi, '/posts')
 api.add_resource(PostApi, '/posts/<uuid>')
+api.add_resource(UserListApi, '/users')
